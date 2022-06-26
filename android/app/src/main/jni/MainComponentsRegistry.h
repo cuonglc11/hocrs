04437d7a14c72a6960c6ef8b1c5922af8f9ef1b6
unknown <cuonglc11>
2022-06-26 20:18:02 +0700

    code

--- android/app/src/main/jni/MainComponentsRegistry.h
+++ android/app/src/main/jni/MainComponentsRegistry.h
@@ -13,7 +13,7 @@ class MainComponentsRegistry
  public:
   // Adapt it to the package you used for your Java class.
   constexpr static auto kJavaDescriptor =
-      "Lcom/api_axios/newarchitecture/components/MainComponentsRegistry;";
+      "Lcom/hocreduxthunk/newarchitecture/components/MainComponentsRegistry;";
 
   static void registerNatives();
 
